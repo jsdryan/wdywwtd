54e822d33f9353c5b9fbf3ed35938239cc8263a2
Charlotte Zhang <jsdryan@gmail.com>
2022-02-03 19:40:26 +0800

    改用 axios 取得抽取番號來源。

--- src/g.js
+++ src/g.js
@@ -1,16 +1,22 @@
-const got = require('got');
+const axios = require('axios');
 const cheerio = require('cheerio');
 const arrayToTxtFile = require('array-to-txt-file');
+
+axios.defaults.withCredentials = true;
 
 const getAllVideoId = async () => {
   const data = [];
   for (let pageNum = 1; pageNum <= 25; pageNum++) {
     console.log(`Page ${pageNum}`);
-    const res = await got(`https://www.javlibrary.com/tw/vl_mostwanted.php`, {
-      searchParams: { mode: 1, page: pageNum },
-      headers: { Cookie: 'over18=18', 'user-agent': 'Android' },
-    });
-    const $ = cheerio.load(res.body);
+    const res = await axios.get(
+      'https://www.javlibrary.com/tw/vl_mostwanted.php',
+      {
+        method: 'GET',
+        headers: { Cookie: 'over18=18', 'user-agent': 'Android' },
+        params: { mode: 1, page: 1 },
+      }
+    );
+    const $ = cheerio.load(res.data);
     const videoItems = $('.video > a > .id');
     console.log(`Video items QTY: ${videoItems.length}`);
     for (let videoIndex = 0; videoIndex < videoItems.length; videoIndex++) {
