3b397530102191d993f3689fcd4a7bfaca047b2e
Charlotte Zhang <jsdryan@gmail.com>
2022-02-16 00:02:00 +0800

    Add new ganhua.

--- src/ganHuaArray.js
+++ src/ganHuaArray.js
@@ -9,6 +9,9 @@ function randomGanHua() {
     '不是不跟你回家，是你沒有開瑪莎。',
     '不是緣分輕易流逝，而是你沒有開賓士。',
     '我不是渣，我只是想讓女孩都有個家。',
+    '妳問我這張照片像不像網美，我覺得有妳的風景就是完美。',
+    '睡前山盟海誓，醒來請問妳是？',
+    '我覺得我們可以浪漫一點，妳浪一點，我慢一點',
   ];
   const ganHuaArrayLength = ganHuaArray.length;
   return ganHuaArray[_.random(0, ganHuaArrayLength - 1)];
